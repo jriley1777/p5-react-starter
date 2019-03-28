71c40e75a8115b6f5f43bde16f8ec1487c2ad184
Joseph Riley <jriley@pandora.com>
2019-03-27 18:32:26 -0700

    initial commit; add dependencies and sample sketch with wrapper component;

---
+++ src/components/Processing/sketches/sketch.js
@@ -0,0 +1,62 @@
+export default function(p) {
+    let w, h;
+    let letters, text1, text2;
+
+    let theta = 0.0; // Start angle at 0
+    let amplitude = 50.0; // Height of wave
+    let period = 500.0; // How many pixels before the wave repeats
+    let dx = (p.TWO_PI / period);
+
+
+    p.setup = function() {
+        w = p.max(window.innerWidth);
+        h = p.max(window.innerHeight);
+        p.createCanvas(w, h);
+        p.frameRate(60);
+        letters = new TextArray("HelloWorld!", 0.75);
+        text1 = new TextArray("Wavey p5 starter template!", 0.25, 200);
+        text2 = new TextArray("get groooooovin", 0.25, h-200);
+    };
+
+    p.draw = function() {
+        p.background(255);
+        p.fill(0);
+        letters.draw();
+        p.fill(255,0,0);
+        text1.draw();
+        text2.draw();
+    };
+
+    let TextArray = function(string, magic, yOffset=h/2) {
+        this.letterArray = string.split("");
+        this.letterObjs = this.letterArray.map((x, i) => {
+            let letterXPos = (i - this.letterArray.length/2 ) * 50;
+            return {
+                letter: x,
+                location: p.createVector(w/2 + letterXPos, yOffset)
+            }
+        });
+
+        this.draw = function(){
+            this.letterObjs.map(x => {
+                p.textFont('Avenir Next', 56);
+                p.text(x.letter, x.location.x, x.location.y);
+                return null;
+            });
+            this.update();
+        };
+        this.update = function(){
+            // Increment theta (try different values for
+            // 'angular velocity' here)
+            theta += 0.01;
+
+            // For every x value, calculate a y value with sine function
+            let x = theta;
+            this.letterObjs.map(letter => {
+                letter.location.y = yOffset  + (p.sin(x) * amplitude);
+                x += (dx + magic);
+                return null;
+            });
+        };
+    }
+}
